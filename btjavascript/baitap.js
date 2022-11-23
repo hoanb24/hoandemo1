2b14893688ee87bb87786d82de2b27bb9b940e4a
HoVanDi <116499784+HoVanDi@users.noreply.github.com>
2022-11-23 10:13:19 +0700

    xong

---
+++ btjavascript/baitap.js
@@ -0,0 +1,9 @@
+    var a = "Hello";
+    var b = "World";
+    var c = a + " " + b;
+    // alert(c);
+    // document.write(c)
+    // document.getElementById('kq').innerHTML=c
+    document.getElementById("demo1").innerHTML="Tai lieu hoc CSS";
+    document.getElementById("demo2").innerHTML="Tai lieu hoc MySQL";
+    document.getElementById("demo3").innerHTML="Tai lieu hoc PHP";
